446f0a1269d02e331c50b233aba37853fe2ceeea
nj442442-collab <nj442442@gmail.com>
2025-10-16 10:41:50 -0500

    convert text to lowercase, remove punctuation and split the text

--- text.stats.py
+++ text.stats.py
@@ -1,1 +1,11 @@
 text = "Cats and dogs are fun, but cats are definitely funnier than dogs."
+
+def main():
+    lower_case = text.lower()
+    for punctuation in ".,!?;:":
+        lower_case.replace(punctuation," ")
+    split_words = lower_case.split()
+
+
+if __name__ == "__main__":
+    main()
