5ecf65beb62623347780bdc14f15fff41ae2084d
nj442442-collab <nj442442@gmail.com>
2025-10-17 14:50:55 -0500

    add function to determine the most common word

--- text.stats.py
+++ text.stats.py
@@ -10,11 +10,16 @@ def main():
 
     print(words)
 
-    unique_word_count = get_unique_word_count(words)
+    unique_word_count = get_unique_word_count(words) #number of word appear without similar word
     print(f"UNIQUE_WORDS: {unique_word_count}")
 
-    longest_word = get_longest_word(words)
-    print(f"LONGEST_WORD: {longest_word}")
+    longest_word = get_longest_word(words) 
+    print(f"LONGEST_WORD: {longest_word}") #print the longest word
+
+    most_common_word = get_most_common_word(words)
+    print(f"MOST_COMMON_WORD_COUNT: {most_common_word}")
+
+
 
 
 
@@ -29,7 +34,23 @@ def get_longest_word(words): # function for determine the longest word
             longest_word = word
     return longest_word
     
+def get_most_common_word(words):
+    
+    word_count = {} 
+    for word in words:#count the number of each word
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
 
+    #determine the most common word
+    max_word = None
+    max_count = 0
+    for word, count in word_count.items():
+        if count > max_count:
+            max_word = word
+            max_count = count
+    return max_word
 
 if __name__ == "__main__":
     main()
