72d13d2c1d794305ca0e7c10715a57ab06297718
nj442442-collab <nj442442@gmail.com>
2025-10-17 14:10:56 -0500

    add function to determine the longest word

--- text.stats.py
+++ text.stats.py
@@ -13,11 +13,23 @@ def main():
     unique_word_count = get_unique_word_count(words)
     print(f"UNIQUE_WORDS: {unique_word_count}")
 
+    longest_word = get_longest_word(words)
+    print(f"LONGEST_WORD: {longest_word}")
+
 
 
 def get_unique_word_count(words): #find how many unique words appear
     unique_word = set(words)
     return len(unique_word)
+
+def get_longest_word(words): # function for determine the longest word
+    longest_word = words[0]
+    for word in words:
+        if len(longest_word) < len(word):
+            longest_word = word
+    return longest_word
+    
+
 
 if __name__ == "__main__":
     main()
