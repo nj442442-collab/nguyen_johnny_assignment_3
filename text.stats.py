c77a4d54fe57f67b1458e8bd64eb804958d7e672
nj442442-collab <nj442442@gmail.com>
2025-10-17 13:52:14 -0500

    fix some code to make the convert, remove, and split more effective

--- text.stats.py
+++ text.stats.py
@@ -1,10 +1,12 @@
 text = "Cats and dogs are fun, but cats are definitely funnier than dogs."
 
 def main():
-    lower_case = text.lower()
+    lower_case = text.lower() #convert letter to lower case
+    
     for punctuation in ".,!?;:":
-        lower_case.replace(punctuation," ")
-    split_words = lower_case.split()
+        lower_case = lower_case.replace(punctuation," ") # remove puctuation and space
+    
+    words = lower_case.split() #split a hold sentence into each word
 
 
 if __name__ == "__main__":
