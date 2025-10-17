b41c8080d3d1a495414cbc6f5bcc9a7f5ce60a1e
nj442442-collab <nj442442@gmail.com>
2025-10-17 14:01:46 -0500

    add the function to count the unique number of the sentence.

--- text.stats.py
+++ text.stats.py
@@ -8,6 +8,16 @@ def main():
     
     words = lower_case.split() #split a hold sentence into each word
 
+    print(words)
+
+    unique_word_count = get_unique_word_count(words)
+    print(f"UNIQUE_WORDS: {unique_word_count}")
+
+
+
+def get_unique_word_count(words): #find how many unique words appear
+    unique_word = set(words)
+    return len(unique_word)
 
 if __name__ == "__main__":
     main()
